a5ab0e8be7bfd479ec54fe741058513dd7d9d949
kshitiz vikram singh <kshitizvikramsingh123@gmail.com>
2023-10-29 19:50:20 +0530

    Created the homepage

---
+++ app.js
@@ -0,0 +1,39 @@
+const express=require("express")
+
+const path=require("path")
+
+const app= express()
+
+const views=path.join(__dirname,"/views")
+
+const publicdir=path.join(__dirname,"public")
+
+app.use(express.static(publicdir))
+
+app.set("view engine","ejs")
+app.set("views", views)
+
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen("80",()=>{
+    console.log("App is up on port 80")
+})
